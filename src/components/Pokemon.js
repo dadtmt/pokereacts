de271fb53b8826f110bcd83e27d063583de5e274
tmt <thomas.culdaut@gmail.com>
2018-05-16 14:53:01 +0200

    pokeapi

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,12 +3,15 @@ import React,{ Component } from 'react'
 class Pokemon extends Component {
 
     render() {
-        return <div>Pokemon</div>
+        return <div>Pokemon:</div>
     }
 
     componentDidMount() {
         console.log('pokemon did mount')
-
+        const url = 'https://pokeapi.co/api/v2/pokemon/pikachu'
+        fetch(url)
+        .then(res => res.json())
+        .then(pokemon => console.log(pokemon))
     }
 
 }
