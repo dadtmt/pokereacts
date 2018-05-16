7f769ef0ce9c41affec861938017bdd0c48cd93e
tmt <thomas.culdaut@gmail.com>
2018-05-16 16:04:24 +0200

    prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
-import Pokemon from './components/Pokemon'
+import Pokemon from "./components/Pokemon"
 
 class App extends Component {
   render() {
@@ -14,8 +14,8 @@ class App extends Component {
         </header>
         <Pokemon />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,36 +1,38 @@
-import React,{ Component } from 'react'
+import React, { Component } from "react"
 
 class Pokemon extends Component {
+  constructor() {
+    super()
 
-    constructor() {
-        super()
-        this.state = {
-            loading: true,
-            pokemon: {
-                name: ''
-            }
-        }
+    this.state = {
+      loading: true,
+      pokemon: {
+        name: ""
+      }
     }
-    render() {
-        return <div>
-            { this.state.loading && <p>Loading</p> }
-            <p>Pokemon: {this.state.pokemon.name}</p>
-        </div>
-    }
+  }
+  render() {
+    return (
+      <div>
+        {this.state.loading && <p>Loading</p>}
+        <p>Pokemon: {this.state.pokemon.name}</p>
+      </div>
+    )
+  }
 
-    componentDidMount() {
-        console.log('pokemon did mount')
-        const url = 'https://pokeapi.co/api/v2/pokemon/pikachu'
-        fetch(url)
-        .then(res => res.json())
-        .then(pokemonRes => this.setState(
-            prevState => ({ 
-                loading: false,
-                pokemon: pokemonRes 
-            })
-        ))
-    }
+  componentDidMount() {
+    console.log("pokemon did mount")
 
+    const url = "https://pokeapi.co/api/v2/pokemon/pikachu"
+    fetch(url)
+      .then(res => res.json())
+      .then(pokemonRes =>
+        this.setState(prevState => ({
+          loading: false,
+          pokemon: pokemonRes
+        }))
+      )
+  }
 }
 
 export default Pokemon
