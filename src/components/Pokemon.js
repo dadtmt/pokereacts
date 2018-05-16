70e999a40ee7855b82f0243496df7fa4cbe4107b
tmt <thomas.culdaut@gmail.com>
2018-05-16 14:47:16 +0200

    didmount

---
+++ src/components/Pokemon.js
@@ -0,0 +1,16 @@
+import React,{ Component } from 'react'
+
+class Pokemon extends Component {
+
+    render() {
+        return <div>Pokemon</div>
+    }
+
+    componentDidMount() {
+        console.log('pokemon did mount')
+
+    }
+
+}
+
+export default Pokemon
