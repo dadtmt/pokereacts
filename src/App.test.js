58e8e9dc3c2360bc5c8d9fc94d10e016940d13ac
tmt <thomas.culdaut@gmail.com>
2018-05-17 14:05:54 +0200

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// the expected response from the API
+const fakePokemon = {
+  name: "Fakichu"
+}
+
+// mock (simulate) the http response
+const mockResponse = (status, statusText, response) => {
+  return new Response(response, {
+    status: status,
+    statusText: statusText,
+    headers: {
+      "Content-type": "application/json"
+    }
+  })
+}
+
+// a fake promise that returns the fake response
+const fakePromise = Promise.resolve(
+  mockResponse(200, null, JSON.stringify({ fakePokemon }))
+)
+
+// replace the fetch from the browser by our fake implementation
+window.fetch = jest.fn().mockImplementationOnce(() => {
+  return fakePromise
+})
+
+it("renders without crashing", () => {
+  const div = document.createElement("div")
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
